045c93a7070a39e737755e3213ae13462c267887
LeoGarzon <leogarzons90@gmail.com>
2024-02-22 19:14:48 -0500

    first commit

---
+++ config/config.js
@@ -0,0 +1,6 @@
+export const API_KEY = `24b49a579b23402d973e715dfb8ebd9a`;
+export const endpoint = `https://newsapi.org/v2/top-headlines`;
+export const category = 'general';
+export const country = 'us'
+
+
---
+++ screens/All.js
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from 'react'
+import { View, Text, StyleSheet } from 'react-native';
+import { NativeBaseProvider, FlatList, Divider, Image, Spinner } from 'native-base';
+import { services } from '../services/services';
+import moment from 'moment'
+export default function Home() {
+    const [newsData, setNewsData] = useState([])
+    useEffect(() => {
+        services('general')
+            .then(data => {
+                setNewsData(data)
+            })
+            .catch(error => {
+                alert(error)
+            })
+    }, [])
+    return (
+        <NativeBaseProvider>
+    {newsData.length > 1 ? (
+        <FlatList
+            data={newsData}
+            renderItem={({ item }) => (
+                <View>
+                    <View style={styles.newsContainer}>
+                        <Image
+                            style={styles.Image}
+                            source={{ uri: item.urlToImage }}
+                            alt={item.title}
+                        />
+                        <Text style={styles.title}>
+                            {item.title}
+                        </Text>
+                        <Text style={styles.date}>
+                            {moment(item.publishedAt).format('LLL')}
+                        </Text>
+                        <Text style={styles.newsDescription}>
+                            {item.description}
+                        </Text>
+                    </View>
+                    <Divider my={2} bg="#e0e0e0" />
+                </View>
+            )}
+            keyExtractor={(_item, index) => index.toString()}
+        />
+    ) : (
+        <View style={styles.spinner}>
+            <Spinner color="danger.400" />
+        </View>
+    )}
+</NativeBaseProvider>
+
+
+    )
+}
+
+const styles = StyleSheet.create({
+    newsContainer: {
+        padding: 10
+    },
+    title: {
+        fontSize: 18,
+        marginTop: 10,
+        fontWeight: "600"
+    },
+    newsDescription: {
+        fontSize: 16,
+        marginTop: 10
+    },
+    date: {
+        fontSize: 14
+    },
+    spinner: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 400
+    },
+
+    Image: {
+        width: '100%',
+        height: 250,
+        margin: 7
+    }
+});
